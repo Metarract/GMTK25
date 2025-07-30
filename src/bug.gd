22697c7bdee246119be3d9a632d3cd3f716d560d
NonMajorNerd <38517935+NonMajorNerd@users.noreply.github.com>
2025-07-30 16:23:23 -0500

    Bug Stats Popup
    Bug Stats Popup menu. Set up as PanelContainer with loadBug(Bug) function. Small changes to bug class correcting for default init values.

--- src/bug.gd
+++ src/bug.gd
@@ -7,19 +7,18 @@ var description:String = ""
 var texture:Texture2D = null
 var color:Color = Color(1.0, 1.0, 1.0, 1.0)
 var weight:float = 0.0
-var movement_speed:float = 0.0 # likely to move to MovementController ?
-
+var movement_speed:float = 0.0 # likely to move to MovementController
 var base_trade_value:float = 10.0
 var trade_value:float:
 	set(v):
 		print('eh? should we be setting Bug.trade_value directly?')
 		trade_value = v
 	get():
-		return base_trade_value + ((10 * affability) + (10 * crunch) + (10 * honor) + (10 * juice) + (10 * leg) + (10 * stink))
+		return base_trade_value + ((10 * affability) + (10 * cronch) + (10 * honor) + (10 * juice) + (10 * leg) + (10 * stink))
 
 # stats
 var affability:float = 0.0
-var crunch:float = 0.0
+var cronch:float = 0.0
 var honor:float = 0.0
 var juice:float = 0.0
 var leg:int = 0 # literally the number of legs the bug has
@@ -30,7 +29,7 @@ var sprite:Sprite2D = null
 var collision_shape:CollisionObject2D = null
 var movement_controller:Node = null
 
-func _init(bn:String, d:String, t:Texture2D, wgt:float, ms:float, btv:float, col:Color, a:float, c:float, h:float, j:float, l:int, s:float) -> void:
+func _init(bn:String, d:String, t:Texture2D, wgt:float, ms:float, btv:float, col:Color=Color.TRANSPARENT, a:float=-1, c:float=-1, h:float=-1, j:float=-1, l:int=-1, s:float=-1) -> void:
 	bug_name = bn
 	description = d
 	texture = t
@@ -39,25 +38,25 @@ func _init(bn:String, d:String, t:Texture2D, wgt:float, ms:float, btv:float, col
 	base_trade_value = btv
 	
 	# Assign values if passed, or define random ranges here
-	if col: color = col
+	if col != Color.TRANSPARENT: color = col
 	else: color = Color(randf_range(0.0, 1.0), randf_range(0.0, 1.0), randf_range(0.0, 1.0), 1.0)
 	
-	if a: affability = a
+	if a != -1: affability = a
 	else: affability = randf_range(0.0, 10.0)
 	
-	if c: crunch = c
-	else: crunch = randf_range(0.0, 10.0)
+	if c != -1: cronch = c
+	else: cronch = randf_range(0.0, 10.0)
 	
-	if h: honor = h
+	if h != -1: honor = h
 	else: honor = randf_range(0.0, 10.0)
 	
-	if j: juice = j
+	if j != -1: juice = j
 	else: juice = randf_range(0.0, 10.0)
 	
-	if l: leg = l
+	if l != -1: leg = l
 	else: leg = randf_range(0.0, 10.0)
 	
-	if s: stink = s
+	if s != -1: stink = s
 	else: stink = randf_range(0.0, 10.0)
 
 func _ready() -> void:

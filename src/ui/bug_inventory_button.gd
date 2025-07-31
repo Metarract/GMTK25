dc49c4940702c1390b233736b135893fc117952b
NonMajorNerd <38517935+NonMajorNerd@users.noreply.github.com>
2025-07-30 20:08:00 -0500

    Bug Inventory Menu
    Bug Inventory Menu, Bug Inventory Button, and some changes to the bug class that I swore I had made earlier. :)

---
+++ src/ui/bug_inventory_button.gd
@@ -0,0 +1,18 @@
+extends Button
+
+signal bug_inventory_button_pressed
+
+var assigned_bug:Bug:
+	set(b):
+		assigned_bug = b
+		load_bug()
+
+func load_bug() -> bool:
+	if not assigned_bug: return false
+
+	icon = load(assigned_bug.texture_path)
+	add_theme_color_override("icon_normal_color", assigned_bug.color)
+	return true
+
+func _on_pressed() -> void:
+	if assigned_bug: emit_signal("bug_inventory_button_pressed", assigned_bug)
